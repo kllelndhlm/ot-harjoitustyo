55205ef67b45f57a561b6d41635129ac9dd5a377
Kalle Lindholm <kalle.lindholm@helsinki.fi>
2021-05-04 23:20:56 +0300

    Yliviivaus

--- src/services/edit_service.py
+++ src/services/edit_service.py
@@ -3,11 +3,16 @@ from tkinter import ttk, constants
 from tkinter import *
 
 class EditService:
+    """Tekstin muokkauksesta vastaavat funktiot
+    """
     def __init__(self, root):
         self._root = root
         self._frame = None
 
     def selection_indices(self):
+        """Muodostaa tekstin muokkauksessa tarvittavat indeksit
+           graaffisessa käyttöliittymässä valitun tekstin perusteella.
+        """
         self._selection_linestart = self._txt_edit.index("sel.last linestart")
         self._selection_lineend = self._txt_edit.index("sel.last lineend")
         self._selection_start = self._txt_edit.index("sel.first")
@@ -18,6 +23,8 @@ class EditService:
             self._selection_end, END)
 
     def h1_markdown(self):
+        """Muokkaa valitusta tekstistä koon 1 otsikon.
+        """
         self._selection = self._txt_edit.get(
             "sel.last linestart", "sel.last lineend")
         self._remove_markdown()
@@ -28,6 +35,8 @@ class EditService:
         self._txt_edit.tag_remove(SEL, "1.0", END)
 
     def h2_markdown(self):
+        """Muokkaa valitusta tekstistä koon 2 otsikon.
+        """
         self._selection = self._txt_edit.get(
             "sel.last linestart", "sel.last lineend")
         self._remove_markdown()
@@ -38,6 +47,8 @@ class EditService:
         self._txt_edit.tag_remove(SEL, "1.0", END)
 
     def h3_markdown(self):
+        """Muokkaa valitusta tekstistä koon 3 otsikon.
+        """
         self._selection = self._txt_edit.get(
             "sel.last linestart", "sel.last lineend")
         self._remove_markdown()
@@ -47,6 +58,8 @@ class EditService:
         self._txt_edit.tag_remove(SEL, "1.0", END)
 
     def body_markdown(self):
+        """Poistaa mahdollisen muotoilun; tekee valinnasta leipätekstiä.
+        """
         self._selection = self._txt_edit.get(
             "sel.last linestart", "sel.last lineend")
         self._remove_markdown()
@@ -57,6 +70,7 @@ class EditService:
         self._txt_edit.tag_remove(SEL, "1.0", END)
 
     def italic_markdown(self):
+        """Kursivoi tai poistaa aiemman kursivoinnin."""
         self._selection = self._txt_edit.get("sel.first", "sel.last")
         self._selection_indices()
         if self._selection[0] == "_" and self._selection[-1] == "_":
@@ -78,6 +92,7 @@ class EditService:
         self._txt_edit.tag_remove(SEL, "1.0", END)
 
     def bold_markdown(self):
+        """Lihavoi tai poistaa aiemman lihavoinnin"""
         self._selection = self._txt_edit.get("sel.first", "sel.last")
         self._selection_indices()
         if self._selection[0] == "*" and self._selection[-1] == "*":
@@ -98,7 +113,30 @@ class EditService:
                 self._selection_start), "**" + self._selection + "**")
         self._txt_edit.tag_remove(SEL, "1.0", END)
 
+    def strikethrough_markdown(self):
+        """Yliviivaa tai poistaa aiemman yliviivauksen."""
+        self._selection = self._txt_edit.get("sel.first", "sel.last")
+        self._selection_indices()
+        if self._selection[:2] == "~~" and self._selection[-2:] == "~~":
+            while self._selection[:2] == "~~":
+                self._selection = self._selection[2:]
+            while self._selection[-2:] == "~~":
+                self._selection = self._selection[:-2]
+            self._delete_selection()
+            self._txt_edit.insert(self._txt_edit.index(
+                self._selection_start), self._selection)
+        else:
+            while self._selection[:2] == "~~":
+                self._selection = self._selection[3:]
+            while self._selection[:-2] == "~~":
+                self._selection = self._selection[:-2]
+            self._delete_selection()
+            self._txt_edit.insert(self._txt_edit.index(
+                self._selection_start), "~~" + self._selection + "~~")
+        self._txt_edit.tag_remove(SEL, "1.0", END)
+
     def copy_txt(self):
+        """Kopioi tekstin leikepöydälle."""
         self._root.clipboard_clear()
         copied = self._txt_edit.get("1.0", "end-1c")
         self._root.clipboard_append(copied)
