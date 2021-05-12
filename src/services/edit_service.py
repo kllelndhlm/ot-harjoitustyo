fa00703737092d7448b24bb5f2802ab8c076e3a5
Kalle Lindholm <kalle.lindholm@helsinki.fi>
2021-05-13 00:49:23 +0300

    Copypasten karsiminen

--- src/services/edit_service.py
+++ src/services/edit_service.py
@@ -22,14 +22,19 @@ class EditService:
         self._following_selection = self._txt_edit.get(
             self._selection_end, END)
 
-    def h1_markdown(self):
-        """Muokkaa valitusta tekstistä koon 1 otsikon.
+    def h123_sel_rmv_ind_del(self):
+        """Kaikissa otsikkomuotoiluissa kutsutut funktiot
         """
         self._selection = self._txt_edit.get(
             "sel.last linestart", "sel.last lineend")
         self._remove_markdown()
         self._selection_indices()
         self._delete_line_from_result()
+
+    def h1_markdown(self):
+        """Muokkaa valitusta tekstistä koon 1 otsikon.
+        """
+        self._h123_sel_rmv_ind_del()
         self._txt_edit.insert(self._txt_edit.index(
             self._selection_linestart), "# " + self._selection)
         self._txt_edit.tag_remove(SEL, "1.0", END)
@@ -37,11 +42,7 @@ class EditService:
     def h2_markdown(self):
         """Muokkaa valitusta tekstistä koon 2 otsikon.
         """
-        self._selection = self._txt_edit.get(
-            "sel.last linestart", "sel.last lineend")
-        self._remove_markdown()
-        self._selection_indices()
-        self._delete_line_from_result()
+        self._h123_sel_rmv_ind_del()
         self._txt_edit.insert(self._txt_edit.index(
             self._selection_linestart), "## " + self._selection)
         self._txt_edit.tag_remove(SEL, "1.0", END)
@@ -49,10 +50,7 @@ class EditService:
     def h3_markdown(self):
         """Muokkaa valitusta tekstistä koon 3 otsikon.
         """
-        self._selection = self._txt_edit.get(
-            "sel.last linestart", "sel.last lineend")
-        self._remove_markdown()
-        self._delete_line_from_result()
+        self._h123_sel_rmv_ind_del()
         self._txt_edit.insert(self._txt_edit.index(
             self._selection_linestart), "### " + self._selection)
         self._txt_edit.tag_remove(SEL, "1.0", END)
@@ -60,83 +58,48 @@ class EditService:
     def body_markdown(self):
         """Poistaa mahdollisen muotoilun; tekee valinnasta leipätekstiä.
         """
-        self._selection = self._txt_edit.get(
-            "sel.last linestart", "sel.last lineend")
-        self._remove_markdown()
-        self._selection_indices()
-        self._delete_line_from_result()
+        self._h123_sel_rmv_ind_del()
         self._txt_edit.insert(self._txt_edit.index(
             self._selection_linestart), self._selection)
         self._txt_edit.tag_remove(SEL, "1.0", END)
 
-    def italic_markdown(self):
-        """Kursivoi tai poistaa aiemman kursivoinnin."""
+    def italic_bold_strikethrough(self, indx, markdn):
+        """Käsittelee valinnan alkuun ja loppuun tulevat muotoilut
+        """
+        self.indx = indx
+        self.markdn = markdn
         self._selection = self._txt_edit.get("sel.first", "sel.last")
         self._selection_indices()
-        if self._selection[0] == "_" and self._selection[-1] == "_":
-            while self._selection[0] == "_":
-                self._selection = self._selection[1:]
-            while self._selection[-1] == "_":
-                self._selection = self._selection[:-1]
+        if self._selection[:indx] == markdn and self._selection[indx*-1:] == markdn:
+            self._selection = self._selection[indx:]
+            self._selection = self._selection[:indx*-1]
             self._delete_selection()
             self._txt_edit.insert(self._txt_edit.index(
                 self._selection_start), self._selection)
         else:
-            while self._selection[0] == "_":
-                self._selection = self._selection[1:]
-            while self._selection[-1] == "_":
-                self._selection = self._selection[:-1]
             self._delete_selection()
             self._txt_edit.insert(self._selection_start,
-                                  "_" + self._selection + "_")
+                                  markdn + self._selection + markdn)
         self._txt_edit.tag_remove(SEL, "1.0", END)
+
+    def italic_markdown(self):
+        """Kursivoi tai poistaa aiemman kursivoinnin.
+        """
+        self._italic_bold_strikethrough(1, "_")
 
     def bold_markdown(self):
-        """Lihavoi tai poistaa aiemman lihavoinnin"""
-        self._selection = self._txt_edit.get("sel.first", "sel.last")
-        self._selection_indices()
-        if self._selection[0] == "*" and self._selection[-1] == "*":
-            while self._selection[0] == "*":
-                self._selection = self._selection[1:]
-            while self._selection[-1] == "*":
-                self._selection = self._selection[:-1]
-            self._delete_selection()
-            self._txt_edit.insert(self._txt_edit.index(
-                self._selection_start), self._selection)
-        else:
-            while self._selection[0] == "*":
-                self._selection = self._selection[1:]
-            while self._selection[-1] == "*":
-                self._selection = self._selection[:-1]
-            self._delete_selection()
-            self._txt_edit.insert(self._txt_edit.index(
-                self._selection_start), "**" + self._selection + "**")
-        self._txt_edit.tag_remove(SEL, "1.0", END)
+        """Lihavoi tai poistaa aiemman lihavoinnin
+        """
+        self._italic_bold_strikethrough(1, "*")
 
     def strikethrough_markdown(self):
-        """Yliviivaa tai poistaa aiemman yliviivauksen."""
-        self._selection = self._txt_edit.get("sel.first", "sel.last")
-        self._selection_indices()
-        if self._selection[:2] == "~~" and self._selection[-2:] == "~~":
-            while self._selection[:2] == "~~":
-                self._selection = self._selection[2:]
-            while self._selection[-2:] == "~~":
-                self._selection = self._selection[:-2]
-            self._delete_selection()
-            self._txt_edit.insert(self._txt_edit.index(
-                self._selection_start), self._selection)
-        else:
-            while self._selection[:2] == "~~":
-                self._selection = self._selection[3:]
-            while self._selection[:-2] == "~~":
-                self._selection = self._selection[:-2]
-            self._delete_selection()
-            self._txt_edit.insert(self._txt_edit.index(
-                self._selection_start), "~~" + self._selection + "~~")
-        self._txt_edit.tag_remove(SEL, "1.0", END)
+        """Yliviivaa tai poistaa aiemman yliviivauksen.
+        """
+        self._italic_bold_strikethrough(2, "~~")
 
     def copy_txt(self):
-        """Kopioi tekstin leikepöydälle."""
+        """Kopioi tekstin leikepöydälle.
+        """
         self._root.clipboard_clear()
         copied = self._txt_edit.get("1.0", "end-1c")
         self._root.clipboard_append(copied)
